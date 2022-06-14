c443af0062f95a7b99595a54067c59d899c4d4e6
priscila <pitu.ciro@hotmail.comgit config --global user.name pituciro>
2022-06-14 10:04:56 -0300

    hw REACT

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,19 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  //const { max, min, name, img, onClose} = props // destructuring
+  //
+  return (
+    <div>
+        <button onClick={props.onClose}>X</button>
+        <h4>{props.name}</h4>
+        <div> 
+          <p>Min</p>
+          <p>{props.min}</p>
+          <p>Max</p>
+          <p>{props.max}</p>
+        </div>    
+        <img src= {`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt={"img"}/>
+    </div>
+  )
 };
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -2,5 +2,13 @@ import React from 'react';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+    <div>
+      <input type="text" placeholder={"Ciudad..."}/>
+    <button onClick={()=>props.onSearch('Buscando Ciudad')}>Agregar</button>
+    </div>
+  )
 };
+
+// pasar a formulario
+// utilizar el metodo submit del form.
